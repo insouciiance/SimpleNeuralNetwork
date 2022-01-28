26633255c8708d7aeff18bd0a403673d1eab7b93
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-01-28 02:06:05 +0200

    added ImageDataSet

---
+++ SimpleNeuralNetwork/SimpleNeuralNetwork/Data/ImageDataSetEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNeuralNetwork.Data
+{
+    public record ImageDataSetEntry(string Path, bool Truthy);
+}
