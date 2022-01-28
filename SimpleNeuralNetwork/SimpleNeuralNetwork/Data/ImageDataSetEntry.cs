a67416baceb49ed32d7eced317c20a5c8ba44886
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-01-28 02:10:37 +0200

    added GetBitmap(int) method

--- SimpleNeuralNetwork/SimpleNeuralNetwork/Data/ImageDataSetEntry.cs
+++ SimpleNeuralNetwork/SimpleNeuralNetwork/Data/ImageDataSetEntry.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleNeuralNetwork.Data
 {
-    public record ImageDataSetEntry(string Path, bool Truthy);
+    public record ImageDataSetEntry(string Path, bool Truthy)
+    {
+        public Bitmap GetBitmap(int size)
+        {
+            Bitmap original = (Bitmap) Image.FromFile(Path);
+            Bitmap resized = new (original, new Size(size, size));
+            return resized;
+        }
+    }
 }
