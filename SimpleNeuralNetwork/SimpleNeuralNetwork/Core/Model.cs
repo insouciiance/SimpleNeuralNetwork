b77a9b2293bd587d47f78803aa8ca14c71182872
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-01-28 02:42:34 +0200

    started implementing model

---
+++ SimpleNeuralNetwork/SimpleNeuralNetwork/Core/Model.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleNeuralNetwork.Data;
+
+namespace SimpleNeuralNetwork.Core
+{
+    public class Model
+    {
+        public ImageDataSet DataSet { get; }
+
+        public int SidePixelSize { get; }
+
+        public double[] W { get; private set; }
+        
+        public double B { get; private set; }
+
+        public Model(ImageDataSet dataSet, int sidePixelSize = 100)
+        {
+            DataSet = dataSet;
+            SidePixelSize = sidePixelSize;
+        }
+
+        private void Prepare()
+        {
+            W = new double[SidePixelSize * SidePixelSize * 3];
+            B = 0;
+        }
+
+        private void RunForwardPropagation()
+        {
+            
+        }
+    }
+}
